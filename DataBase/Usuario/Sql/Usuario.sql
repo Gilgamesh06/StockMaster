9695b1dede2975729dded881db760647ac358438
Gilgamesh <Gilgamesh06lg@gmail.com>
2024-10-22 07:51:39 -0500

    Correcion del sql de la base de datos usuario.

--- DataBase/Usuario/Sql/Usuario.sql
+++ DataBase/Usuario/Sql/Usuario.sql
@@ -1,36 +1,35 @@
 
--- Creacion de la entidad Persona
+-- Creación de la entidad Persona
 
 CREATE TABLE Persona(
-    id_persona SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
-    documento VARCHAR(15) NOT NULL,
+    documento VARCHAR(15) NOT NULL UNIQUE,
     fechaNacimiento DATE NOT NULL,
-    genero BOOLEAN NOT NULL
-)
+    genero VARCHAR(10) NOT NULL  -- Cambio de genero de tipo bool a tipo String por culpa de las nuevas perpectivas de genero.
+);
 
 -- Creacion de la entidad Empleado
 
 CREATE TABLE Empleado(
-    id_empleado SERIAL PRIMARY KEY,
-    usuario VARCHAR(50) NOT NULL,
-    contraseña VARCHAR(50) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    usuario VARCHAR(50) NOT NULL UNIQUE,
+    clave VARCHAR(255) NOT NULL,  -- Almacena el hash de la password
     salario REAL NOT NULL,
     rol VARCHAR(30) NOT NULL,
     persona_id INT NOT NULL,
-
-    CONSTRAINT fk_persona_id FOREIGN KEY (persona_id) REFERENCES Persona(id_persona)
+    
+    CONSTRAINT fk_persona_id FOREIGN KEY (persona_id) REFERENCES Persona(id)
 );
-
 
 -- Creacion de la entidad Cliente
 
 CREATE TABLE Cliente(
-    id_cliente SERIAL PRIMARY KEY,
-    nit VARCHAR(50) NOT NULL,
-    correo VARCHAR(80) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    nit VARCHAR(50) NOT NULL UNIQUE,
+    correo VARCHAR(80) NOT NULL UNIQUE,
     persona_id INT NOT NULL,
 
-    CONSTRAINT fk_persona_id FOREIGN KEY (persona_id) REFERENCES Persona(id_persona) 
+    CONSTRAINT fk_persona_id FOREIGN KEY (persona_id) REFERENCES Persona(id)
 );
