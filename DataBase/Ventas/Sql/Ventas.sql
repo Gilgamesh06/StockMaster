167b538983c31e78fbc8939fb1d4ac00646a4b0a
Gilgamesh <Gilgamesh06lg@gmail.com>
2024-12-04 15:24:04 -0500

    Modificacion del Script de Ventas

--- DataBase/Ventas/Sql/Ventas.sql
+++ DataBase/Ventas/Sql/Ventas.sql
@@ -1,51 +1,59 @@
--- Crear usuario
+-- Crear usuario si no existe
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE rolname = 'Gilgamesh06') THEN
 
-CREATE USER "Gilgamesh06" WITH PASSWORD 'coplandos';
+      CREATE USER "Gilgamesh06" WITH PASSWORD 'coplandos';
+   END IF;
+END
+$$;
 
---  Crear Rol
+-- Crear base de datos si no existe
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_database
+      WHERE datname = 'ventas') THEN
 
-CREATE ROLE admin;
+      CREATE DATABASE "ventas" OWNER "Gilgamesh06";
+   END IF;
+END
+$$;
 
--- Asignar Permiso a un Rol
-
-ALTER ROLE admin CREATEDB;
-
--- Asignar rol a Usuario
-
-GRANT admin TO "Gilgamesh06";
-
--- Crear database
-
-CREATE DATABASE "ventas" OWNER "Gilgamesh06";
-
--- Creacion de la entidad Factura
-
-CREATE TABLE Factura(
+-- Creación de la entidad factura si no existe
+CREATE TABLE IF NOT EXISTS factura (
     id SERIAL PRIMARY KEY,
     cliente_documento VARCHAR(15) NOT NULL,
     cliente_nit VARCHAR(50) NOT NULL,
-    empleado VARCHAR(100),
+    empleado_documento VARCHAR(100),
     fecha DATE DEFAULT CURRENT_DATE,
-    valor_total REAL NOT NULL,
+    valor_total REAL NOT NULL
 );
 
-CREATE TABLE DetalleFactura(
-    id SERIAL PRIMARY KEY
+-- Creación de la entidad detallefactura si no existe
+CREATE TABLE IF NOT EXISTS detallefactura (
+    id SERIAL PRIMARY KEY,
     producto VARCHAR(255) NOT NULL,
     cantidad INT NOT NULL,
     precio REAL NOT NULL,
     valor REAL NOT NULL,
     id_factura INT,
     
-    CONSTRAINT fk_factura_id FOREIGN KEY (id_factura) REFERENCES Factura(id)
+    CONSTRAINT fk_factura_id FOREIGN KEY (id_factura) REFERENCES factura(id)
 );
 
-CREATE TABLE Devolucion(
+-- Creación de la entidad devolucion si no existe
+CREATE TABLE IF NOT EXISTS devolucion (
     id SERIAL PRIMARY KEY,
     descripcion VARCHAR(255) NOT NULL,
-    fecha DATE DEFAULT CURRENT_DATE
-    empleado VARCHAR(100) NOT NULL,
+    fecha DATE DEFAULT CURRENT_DATE,
+    empleado_documento VARCHAR(100) NOT NULL,
     id_factura INT,
 
-    CONSTRAINT fk_factura_id FOREIGN KEY (id_factura) REFERENCES Factura(id)
+    CONSTRAINT fk_factura_id FOREIGN KEY (id_factura) REFERENCES factura(id)
 );
+
